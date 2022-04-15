8fc50181b8b41bce1d5186ac79b1085655e21353
Timothy Ha <tha10@ucmerced.edu>
2022-04-14 20:15:27 -0700

    fixes scroll delay

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let delay = 1000; // amount of time before scrolling (in ms)
-let distance = 250; // # of pixel to scroll each time
-let threshold = window.innerHeight * 0.3; // how much whitespace before actually scrolling
+const delay = 1000; // amount of time before scrolling (in ms)
+const distance = 250; // # of pixels to scroll each time
+let threshold = window.innerHeight * 0.2; // how much whitespace before actually scrolling
 
 let lastTime = 0;
 let direction = 0;
@@ -11,23 +11,26 @@ function updateDirection(x, time) {
 }
 
 function scroll(direction, distance, time) {
-	if (lastTime + delay < time) {
+	if (time - lastTime > delay) {
 		window.scrollBy({
 			top: direction * distance,
 			behavior: 'smooth'
 		});
 		
-		updateDirection(0, time)
+		updateDirection(0, time);
 	}
 }
 
 webgazer.setGazeListener(function(data, elapsedTime) {
-    if (data == null) return;
+	threshold = window.innerHeight * 0.2;
+	
+	if (data == null) return;
+	
 	if (data.y > window.innerHeight/2 + threshold && direction != 1) updateDirection(1, elapsedTime);
 	else if (data.y < window.innerHeight/2 - threshold && direction != -1) updateDirection(-1, elapsedTime);
-	else if (data < (window.innerHeight/2 + threshold) && data > (window.innerHeight/2 - threshold)) updateDirection(0, 0);
+	else if (data.y < (window.innerHeight/2 + threshold) && data.y > (window.innerHeight/2 - threshold)) updateDirection(0, elapsedTime);
 
 	scroll(direction, distance, elapsedTime);
-
+	
 	console.log(direction);
 }).begin();
